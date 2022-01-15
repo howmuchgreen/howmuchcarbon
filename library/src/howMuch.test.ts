743c14dc496a196f13a8ddbfbbefa0f0be38f570
Thomas CARLI <thomascarli@gmail.com>
2022-01-15 09:08:27 +0100

    Update library/src/howMuch.test.ts
    Co-authored-by: Basile Bruneau <basile@bruneau.email>

--- library/src/howMuch.test.ts
+++ library/src/howMuch.test.ts
@@ -1,7 +1,7 @@
 import { howMuch } from "./howMuch";
 
 describe('howMuch()', () => {
-  it('should return some results when searching for iPad', () => {
+  it('should return some results when searching for iPhone', () => {
     expect(howMuch('iphone').numResults).toBeGreaterThan(0)
   });
 });
